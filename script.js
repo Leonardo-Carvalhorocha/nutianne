d33f3aeac3b35ffe9e5507c4c07d85beb5821459
Leonardo Carvalho <leonardo2001darocha@gmail.com>
2023-10-13 15:05:07 -0300

    ajustando slide mobile e desktop

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 import menuMobile from './js/menu-mobile.js';
-import initSlide from './js/slide.js';
+import slide from './js/slide.js';
+
+const slideDesktop = new slide('.slide-desktop img', '.slide-desktop');
+slideDesktop.init()
+
+const slideMobile = new slide('.slide-mobile img', '.slide-mobile');
+console.log(slideMobile.init())
 
 menuMobile();
-initSlide();
